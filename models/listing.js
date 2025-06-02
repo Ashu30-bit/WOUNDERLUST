8fc2725ec91ec45184afbe720ca4030eaa1cfa97
ashuuuu.30@gmail.com <ashuuuu.30@gmail.com>
2025-06-02 17:18:18 +0530

    initial commit

---
+++ models/listing.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review.js");
+const { required } = require("joi");
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    url: {
+      type: String,
+      required: true,
+    },
+    filename: {
+      type: String,
+      required: true,
+    },
+  },
+  price: Number,
+  location: String,
+  country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  geometry: {
+    type: {
+      type: String, // Don't do `{location: { type: String}}`
+      enum: ["Point"], // 'location.type' must be 'point'
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
+  categry: {
+    type: String,
+    enum: [
+      "Trending",
+      "Room",
+      "Icon cities",
+      "Mountains",
+      "Beach",
+      "Camping",
+      "Farms",
+      "Artics",
+    ],
+  },
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
---
+++ public/js/map.js
@@ -0,0 +1,15 @@
+mapboxgl.accessToken = mapToken;
+
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v11", // you must specify a style
+  center:listing.geometry.coordinates, // [lng, lat]
+  zoom: 9, // starting zoom
+});
+
+// Add marker with popup
+const marker = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(  new mapboxgl.Popup({ offset: 25 })
+  .setHTML(`<h5>${listing.title}</h5><p>Exact location will be provided after booking</p>`))
+  .addTo(map);
